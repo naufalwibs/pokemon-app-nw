d7e9aedbcba22c18a0fdc0647f252771ee46ac08
naufalwibs <naufalwibisono95@gmail.com>
2021-05-09 23:28:07 +0700

    Styling First Step

---
+++ components/PokemonCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-col lg="3" md="4" sm="6" cols="6">
+    <v-card elevation="3" outlined shaped :class="`rounded-bl-xl`" color="blue">
+      <v-card-title> Pokemon Name </v-card-title>
+      <v-img
+        src="https://pokeres.bastionbot.org/images/pokemon/26.png"
+        alt="26"
+      >
+      </v-img>
+    </v-card>
+  </v-col>
+</template>
